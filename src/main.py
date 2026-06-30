14a7e23c74182d94889102c2b75c8ffb2636efac
Daniel <danielmariestrada@gmail.com>
2026-06-30 01:59:22 -0400

    redirect / -> /cafesia

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 from routes import router
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,3 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 app.include_router(router)
+
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/cafesia")
